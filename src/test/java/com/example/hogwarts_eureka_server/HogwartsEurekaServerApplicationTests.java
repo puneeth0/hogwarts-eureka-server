dbd32c779296fb0c74dc72730e7ac7a6e82f96c7
puneeeth0 <puneethshetty0@gmail.com>
2025-01-27 22:47:18 +0530

    first commit

---
+++ src/test/java/com/example/hogwarts_eureka_server/HogwartsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hogwarts_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
